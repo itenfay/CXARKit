5b38bd74d14d5abbcc391073f6386e034bfa72d0
Teng Fei <hansen981@126.com>
2024-04-15 01:25:36 +0800

    commit

---
+++ Example/CXARKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CXARKit
+//
+//  Created by Teng Fei on 04/14/2024.
+//  Copyright (c) 2024 Teng Fei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
